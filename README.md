9f1164cbf9bd9c2626d0ce50a74273a2530d2a15
Dulan Jayasuriya <dulanjayasuriya@gmail.com>
2022-09-17 11:23:03 +0530

    requirements.txt added Configuration added Readme added code refactoring

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
-# prof-search
+# This code will search for professors in [Google-scholar](https://scholar.google.com/citations?view_op=search_authors&hl=en&mauthors=) given a basic search query link
+
+## Install the requirements
+
+```angular2html
+pip install -r requirements.txt
+```
+## Change the config file as your wish
+
+```json
+
+{
+  "search_page_depth": {No of pages needs to search | int},
+  "starting_url": {starting url | string} ,
+  "my_interests": [
+    {List of interests | String}
+  ],
+  "save_filename": {Text file name | string}
+}
+```
+
+Go to the [google scholar profiles page](https://scholar.google.com/citations?view_op=search_authors&hl=en&mauthors=) to get the starting url...
+
+## Run the main.py file
+
+```angular2html
+python3 main.py
+```
+
+You will see the following
+
+![](assets/Screenshot_20220917_111448.png)
+
+Then after the end of the search, the data will be dumped to a text file like below
+
+![](assets/Screenshot_20220917_111704.png)
+
+## Apply for your PHD today! 
--- main.py
+++ main.py
@@ -1,114 +1,90 @@
-# This is a sample Python script.
+# This script is developed by https://github.com/CodeProcessor
+import json
 
 import requests
 from bs4 import BeautifulSoup
 
-STOP_DEPTH = 1000
-MY_INTERESTS = [
-    "machine learning",
-    "statistics",
-    "artificial intelligence",
-    "Data mining",
-    "Information Retrieval",
-    "Deep Learning",
-    "Neural Networks",
-    "computer vision",
-    "pattern recognition"
-]
 
+class ProfSearch:
 
-def get_h_index(profile):
-    r = requests.get(profile)
-    # print(r.content)
-    soup = BeautifulSoup(r.content, 'lxml')
-    # print(soup.prettify())
+    def __init__(self):
+        self.config = {}
+        with open("config.json", "r") as fp:
+            self.config = json.load(fp)
 
-    table = soup.find('table', attrs={'id': 'gsc_rsb_st'})
-    # print(table.prettify())
-    for row in table.findAll('tr'):
-        # row_val = row.find('a').string
-        # print(row)
-        row_text = [td.text for td in row.findAll('td')]
-        if len(row_text) > 0 and row_text[0] == "h-index":
-            return row_text[1]
+    def get_h_index(self, profile):
+        r = requests.get(profile)
+        soup = BeautifulSoup(r.content, 'lxml')
+        table = soup.find('table', attrs={'id': 'gsc_rsb_st'})
+        for row in table.findAll('tr'):
+            row_text = [td.text for td in row.findAll('td')]
+            if len(row_text) > 0 and row_text[0] == "h-index":
+                return row_text[1]
 
+    def main(self):
+        starting_url = self.config["starting_url"]
+        prof_list = []
+        try:
+            depth = 0
+            no_of_profs = 0
+            while True:
+                depth += 1
+                r = requests.get(starting_url)
+                soup = BeautifulSoup(r.content, 'lxml')
 
-def main(starting_url):
-    prof_list = []
-    try:
-        depth = 0
-        no_of_profs = 0
-        while True:
-            depth += 1
-            r = requests.get(starting_url)
-            # print(r.content)
-            soup = BeautifulSoup(r.content, 'lxml')
-            # print("Encoding method :", soup.original_encoding)
+                profs = soup.findAll('div', attrs={'class': 'gs_ai gs_scl gs_ai_chpr'})
+                for prof in profs:
+                    no_of_profs += 1
+                    prof_name = prof.img['alt']
+                    bs_interests = prof.findAll('a', attrs={'class': 'gs_ai_one_int'})
+                    prof_profile = f"https://scholar.google.com{prof.a['href']}"
+                    interests = []
+                    for bs_interest in bs_interests:
+                        interests.append(bs_interest.string.lower())
 
-            # print(soup.prettify())
+                    my_interests = self.config["my_interests"]
+                    my_list = [my_interest.lower() in interests for my_interest in my_interests]
+                    if any(my_list):
+                        print(
+                            f"prof {prof_name} Found with interest fields : {[val for ok, val in zip(my_list, my_interests) if ok]}")
+                        h_index = self.get_h_index(prof_profile)
+                        print(f"H-index: {h_index}")
+                        prof_list.append(
+                            {
+                                "name": prof_name,
+                                "interests": interests,
+                                "h-index": h_index,
+                                "profile": prof_profile
+                            }
+                        )
 
-            profs = soup.findAll('div', attrs={'class': 'gs_ai gs_scl gs_ai_chpr'})
-            for prof in profs:
-                no_of_profs += 1
-                prof_name = prof.img['alt']
-                bs_interests = prof.findAll('a', attrs={'class': 'gs_ai_one_int'})
-                prof_profile = f"https://scholar.google.com{prof.a['href']}"
-                interests = []
-                for bs_interest in bs_interests:
-                    interests.append(bs_interest.string.lower())
+                try:
+                    next = soup.find('button', attrs={'aria-label': 'Next'})
+                    onclick = next.attrs["onclick"]
+                    url_pos = onclick.split("/")[-1]
+                    replaced_url = url_pos.replace("\\x26", "&").replace("\\x3d", "=")
+                    full_url = f"https://scholar.google.com/{replaced_url}"
 
-                my_list = [my_interest.lower() in interests for my_interest in MY_INTERESTS]
-                if any(my_list):
-                    print(
-                        f"prof {prof_name} Found with interest fields : {[val for ok, val in zip(my_list, MY_INTERESTS) if ok]}")
-                    h_index = get_h_index(prof_profile)
-                    print(f"H-index: {h_index}")
-                    prof_list.append(
-                        {
-                            "name": prof_name,
-                            "interests": interests,
-                            "h-index": h_index,
-                            "profile": prof_profile
-                        }
-                    )
+                    starting_url = full_url
+                except (AttributeError, KeyError):
+                    break
 
-            try:
-                next = soup.find('button', attrs={'aria-label': 'Next'})
-                onclick = next.attrs["onclick"]
-                url_pos = onclick.split("/")[-1]
-                replaced_url = url_pos.replace("\\x26", "&").replace("\\x3d", "=")
-                full_url = f"https://scholar.google.com/{replaced_url}"
-                # print(full_url)
+                print(f"Page {depth} total professors searched: {no_of_profs}")
+                if depth > self.config["search_page_depth"]:
+                    break
+        except KeyboardInterrupt:
+            pass
 
-                starting_url = full_url
-                # print(json.dumps(prof_dict, indent=4))
-                # break
-            except (AttributeError, KeyError):
-                break
-
-            print(f"Page {depth} total professors searched: {no_of_profs}")
-            if depth > STOP_DEPTH:
-                break
-    except KeyboardInterrupt:
-        pass
-
-    print(f"     {'PROF NAME':<25} | {'h-index':<10} |Interests")
-    with open("results.txt", 'a') as fp:
-        for i, p in enumerate(prof_list):
-            print(f"{i:2d} | {p['name']:<25} | {p['h-index']:<10} | {','.join(p['interests']):<50} | {p['profile']}")
-            fp.write(
-                f"{i},{p['name']},{p['h-index']},{' | '.join(p['interests'])},{p['profile']}\n"
-            )
+        print(f"     {'PROF NAME':<25} | {'h-index':<10} |Interests")
+        with open(self.config["save_filename"], 'a') as fp:
+            for i, p in enumerate(prof_list):
+                print(
+                    f"{i:2d} | {p['name']:<25} | {p['h-index']:<10} | {','.join(p['interests']):<50} | {p['profile']}")
+                fp.write(
+                    f"{i},{p['name']},{p['h-index']},{' | '.join(p['interests'])},{p['profile']}\n"
+                )
 
 
 if __name__ == '__main__':
-    uni_of_mel = "https://scholar.google.com/citations?view_op=view_org&hl=en&org=10194437184893062620"
-    # main_url = "https://scholar.google.com/citations?hl=en&view_op=search_authors&mauthors=university+of+melbourn&btnG="
-    # # main_url = "https://scholar.google.com/citations?hl=en&view_op=search_authors&mauthors=Computer+Science%2C+University+of+Melbourne&btnG="
-    # # URL = "https://scholar.google.com/citations?view_op=search_authors&hl=en&oe=ASCII&mauthors=university+of+melbourn&after_author=ZTrkAK62_v8J&astart=10"
-
-    main(uni_of_mel)
-    # h_index = get_h_index("https://scholar.google.com/citations?hl=en&user=Vt5edEkAAAAJ")
-    # print(h_index)
-.0
-32
+    prof_object = ProfSearch()
+    prof_object.main()
